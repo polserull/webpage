ae3c91da56f6d5ae73d92395d7a4089dd924e2ce
polserull <88738134+polserull@users.noreply.github.com>
2024-05-15 17:42:46 +0100

    .
    .

---
+++ scripts/markers.js
@@ -0,0 +1,44 @@
+L.marker([52.813749, -1.637174], {icon: funnelCloudMarker}).addTo(map).bindPopup(`<pre>Possible Funnel Cloud 
+Location: Over Burton upon Trent 
+[52.813749, -1.637174] [06-05-24 6:34BST]
+Source: Twitter | <a href="https://twitter.com/sodafalls/status/1787536503842668729?ref_src=twsrc%5Etfw%7Ctwcamp%5Etweetembed%7Ctwterm%5E1787536503842668729%7Ctwgr%5E0062d3ab9c1da29e6ff51148a63fd22cfa43a6bb%7Ctwcon%5Es1_&ref_url=https%3A%2F%2Fwww.torro.org.uk%2FIPS%2Findex.php%3Fapp%3Dcoremodule%3Dsystemcontroller%3Dembedurl%3Dhttps%3A%2F%2Ftwitter.com%2Fsodafalls%2Fstatus%2F1787536503842668729">Source</a></pre>`);
+L.marker([52.774867077656864, -1.557549742237998], {icon: funnelCloudMarker}).addTo(map).bindPopup(`<pre>Possible Funnel Cloud 
+Location: Swadlincote, Derbyshire 
+[52.774867077656864, -1.557549742237998] [06-05-24 7:12BST]
+Source: Twitter | <a href="https://twitter.com/uktornados/status/1787546069720514730?ref_src=twsrc%5Etfw%7Ctwcamp%5Etweetembed%7Ctwterm%5E1787546069720514730%7Ctwgr%5Ef701bb51e263063d25d14837ecad57d9136e12c1%7Ctwcon%5Es1_&ref_url=https%3A%2F%2Fwww.torro.org.uk%2FIPS%2Findex.php%3Fapp%3Dcoremodule%3Dsystemcontroller%3Dembedurl%3Dhttps%3A%2F%2Ftwitter.com%2Fuktornados%2Fstatus%2F1787546069720514730">Source</a></pre>`);
+L.marker([54.66109117051964, -2.2028157305819076], {icon: funnelCloudMarker}).addTo(map).bindPopup(`<pre>Possible Funnel Cloud
+Estimate Location: B6277 Highway 
+[54.66109117051964, -2.2028157305819076] [06-05-24 15:30BST]
+Source: Facebook | <a href="https://www.facebook.com/watch/?v=746529750837359">Source</a></pre>`);
+L.marker([51.77368325105944, -3.245661634061207], {icon: funnelCloudMarker}).addTo(map).bindPopup(`<pre>Possible Funnel Cloud 
+Location: Tredegar
+[51.77368325105944, -3.245661634061207] [07-05-24 16:40BST]
+Source: Twitter | <a href="https://twitter.com/McCarthyKarl/status/1787873989118873753?ref_src=twsrc%5Etfw%7Ctwcamp%5Etweetembed%7Ctwterm%5E1787873989118873753%7Ctwgr%5E9011092b919548078e0209e85ed21b27a852802a%7Ctwcon%5Es1_&ref_url=https%3A%2F%2Fwww.torro.org.uk%2FIPS%2Findex.php%3Fapp%3Dcoremodule%3Dsystemcontroller%3Dembedurl%3Dhttps%3A%2F%2Ftwitter.com%2FMcCarthyKarl%2Fstatus%2F1787873989118873753">Source</a></pre>`);
+L.marker([51.49774320425926, -0.6369741664371422], {icon: damagingLightningMarker}).addTo(map).bindPopup(`<pre>Damaging Lightning
+Estimate Location: Eton Wick
+[51.49774320425926, -0.6369741664371422] [02-05-24 N/A]
+Source: News | <a href="https://www.windsorexpress.co.uk/gallery/appeals-and-information/196332/firefighters-tackle-eton-wick-loft-fire-caused-by-a-lightning-strike.html">Source</a><pre>`);
+L.marker([50.823979952034136, 0.23899878220061452], {icon: damagingLightningMarker}).addTo(map).bindPopup(`<pre>Damaging Lightning
+Estimate Location: Brookside Avenue, Polegate
+[50.823979952034136, 0.23899878220061452] [02-05-24 01:05BST]
+Source: News | <a href="https://www.theargus.co.uk/news/24293336.live-updates-sussex-hit-thunder-lightning/">Source</a></pre>`);
+L.marker([50.84447198006771, -0.7757965582763232], {icon: damagingLightningMarker}).addTo(map).bindPopup(`<pre>Damaging Lightning
+Estimate Location: College Lane, Chichester
+[50.84447198006771, -0.7757965582763232] [02-05-24 02:24BST]
+Source: News | <a href="https://www.theargus.co.uk/news/24293336.live-updates-sussex-hit-thunder-lightning/"><Source</a></pre>`);
+L.marker([50.79254272710808, -0.6104020396560259], {icon: damagingLightningMarker}).addTo(map).bindPopup(`<pre>Damaging Lightning
+Estimate Location: Elmer, Bognor 
+[50.79254272710808, -0.6104020396560259] [02-05-24 1:44BST]
+Source: News | <a href="https://www.theargus.co.uk/news/24293336.live-updates-sussex-hit-thunder-lightning/">Source</a></pre>`);
+L.marker([57.310200010485474, -2.6233588484252075], {icon: floodingMarker}).addTo(map).bindPopup(`<pre>Flooding
+Estimate Location: Auchleven, Aberdeenshire 
+[57.310200010485474, -2.6233588484252075] [05-05-24 N/A]
+Source: Twitter | <a href="https://twitter.com/Alonso2012F/status/1787180019363487791">Source</a></pre>`);
+L.marker([52.41099743933818, -3.3476055264083038], {icon: floodingMarker}).addTo(map).bindPopup(`<pre>Flooding
+Estimate Location: A483, Llanbadarn Fynydd 
+[52.41099743933818, -3.3476055264083038] [12-05-24 19:00BST]
+Source: News | <a href="https://www.countytimes.co.uk/news/24314854.live-powys-flooding-latest-uk-thunderstorms/">Source</a></pre>`);
+L.marker([53.60835803921186, -2.314070824203321], {icon: damagingLightningMarker}).addTo(map).bindPopup(`<pre>Damaging Lightning
+Location: Lepp Crescent, Bury
+[53.60835803921186, -2.314070824203321] [12-05-24 20:30BST]
+Source: News | <a href="https://www.manchestereveningnews.co.uk/news/greater-manchester-news/we-saw-big-ball-white-29161139">Source</a></pre>`);
